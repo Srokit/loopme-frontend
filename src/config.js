9b85db9acb71dff01816afc40dea1a0a8a6dfeb5
Stan <stan@stansa.dev>
2023-03-29 22:23:49 -0700

    Run remote on prod and local on dev

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 const config = {
-    'apiBaseUrl': 'http://localhost:5002',
+    'apiBaseUrl': (process.env.NODE_ENV === 'production') ? 'https://loopme-backend.herokuapp.com' : 'http://localhost:5002',
 }
 
 export default config;
