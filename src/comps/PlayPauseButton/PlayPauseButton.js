212dd6d86d270912968744395494b4f3cccd1d5d
Stan <stan@stansa.dev>
2023-04-01 01:20:05 -0700

    Playpause better trans

--- src/comps/PlayPauseButton/PlayPauseButton.js
+++ src/comps/PlayPauseButton/PlayPauseButton.js
@@ -1,16 +1,23 @@
 import './PlayPauseButton.css';
 import playImgSrc from '../../images/play.png';
 import pauseImgSrc from '../../images/pause.png';
+import playIconSrc from '../../images/play_icon_1.png';
+import pauseIconSrc from '../../images/pause_icon_1.png';
 import { useContext } from 'react';
 
 import AppContext from '../../AppContext';
 
 const PlayPauseButton = () => {
     const {isPlaying, playOrPause} = useContext(AppContext);
-    const bgImgFn = (isPlaying) ? pauseImgSrc : playImgSrc;
-    const style = {'backgroundImage': `url(${bgImgFn})`};
+    // const bgImgFn = (isPlaying) ? pauseImgSrc : playImgSrc;
+    const bgImgFn = (isPlaying) ? pauseIconSrc : playIconSrc;
+    const playPauseStyle = (isPlaying) ? {'marginLeft': '25px', 'width': '90px', 'height': '100px'} : null;
     return (
-        <div className="playPauseButton" style={style} onClick={playOrPause}></div>
+        <div className="playPauseButtonContainer">
+            <div className="playPauseButton" onClick={playOrPause} style={playPauseStyle}>
+                <img src={bgImgFn} />
+            </div>
+        </div>
     );
 };
 
